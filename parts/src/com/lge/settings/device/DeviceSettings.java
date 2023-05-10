3fccb884885ad2f2c964b769d7ef27fa4b25af29
Bernardo Bas <bernardobas@gmail.com>
2023-05-10 17:21:20 -0300

    msm8996-common: aod: test

--- parts/src/com/lge/settings/device/DeviceSettings.java
+++ parts/src/com/lge/settings/device/DeviceSettings.java
@@ -24,6 +24,7 @@ import android.os.SystemProperties;
 import android.provider.Settings;
 import android.preference.ListPreference;
 import android.preference.Preference;
+import android.preference.Preference.OnPreferenceChangeListener;
 import android.preference.PreferenceCategory;
 import android.preference.EditTextPreference;
 import android.preference.SwitchPreference;
@@ -33,6 +34,7 @@ import android.widget.Toast;
 
 import com.lge.settings.device.Battery;
 import com.lge.settings.device.utils.Constants;
+import com.lge.settings.device.utils.PreferenceHelper;
 import com.lge.settings.device.utils.NodePreferenceActivity;
 
 public class DeviceSettings extends NodePreferenceActivity {
@@ -41,6 +43,8 @@ public class DeviceSettings extends NodePreferenceActivity {
 	private EditTextPreference cyclePreference;
     private EditTextPreference tempPreference;
     private EditTextPreference healthPreference;
+    private ListPreference mAodBacklight;
+    private SwitchPreference mDaylightModeSwitch;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -63,6 +67,15 @@ public class DeviceSettings extends NodePreferenceActivity {
         cyclePreference.setSummary(Battery.getBatteryCycle());
         tempPreference.setSummary(Battery.getBatteryTemp()+"°C");
         healthPreference.setSummary(Battery.getBatteryHealth());
+
+        mAodBacklight = (ListPreference) findPreference(Constants.AOD_KEY);
+        mAodBacklight.setValue(Integer.toString(PreferenceHelper.getAodBacklightType(this)));
+        mAodBacklight.setOnPreferenceChangeListener(this);
+
+        mDaylightModeSwitch = (SwitchPreference) findPreference(Constants.KEY_DLM_SWITCH);
+        mDaylightModeSwitch.setEnabled(DaylightModeSwitch.isSupported());
+        mDaylightModeSwitch.setChecked(DaylightModeSwitch.isCurrentlyEnabled(this));
+        mDaylightModeSwitch.setOnPreferenceChangeListener(new DaylightModeSwitch());
     }
 
     @Override
@@ -74,5 +87,15 @@ public class DeviceSettings extends NodePreferenceActivity {
 
         // mHapticFeedback.setChecked(
         //         Settings.System.getInt(getContentResolver(), KEY_HAPTIC_FEEDBACK, 1) != 0);
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        if (preference == mAodBacklight) {
+            int val = Integer.parseInt((String) newValue);
+            PreferenceHelper.setAodBacklightType(this, val);
+            return true;
+        }
+        return false;
     }
 }
